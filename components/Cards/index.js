c8666f1c8d719c5cad899e7697165a0f775f9f2d
Shafi Masoumi <masoumi2017@icloud.com>
2019-09-14 09:56:25 -0700

    card function created

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,42 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const containerCards = document.querySelector('.cards-container');
+
+axios
+.get('https://lambda-times-backend.herokuapp.com/articles')
+.then(response => {
+    console.log(response)
+})
+.catch(error => {
+    console.log(error + 'Data is returned');
+})
+
+function articleCard(data) {
+    const card = document.createElement('div');
+    const cardHeadline = document.createElement('div');
+    const cardAuthor = document.createElement('div');
+    const imgContainer = document.createElement('div');
+    const cardImage = document.createElement('img');
+    const by = document.createElement('span');
+
+    card.appendChild(cardHeadline);
+    card.appendChild(cardAuthor);
+    author.appendChild(imgContainer);
+    container.appendChild(cardImage);
+    author.appendChild(by);
+
+    card.classList.add('card');
+    cardHeadline.classList.add('headline');
+    cardAuthor.classList.add('author');
+    imgContainer.classList.add('img-container');
+
+    // Adding content //
+    card.textContent = `Card: ${data.articles}`
+    cardHeadline.textContent = `Headline: ${data.articles.headline}`
+    cardAuthor.textContent = `Author: ${data.headers}`
+    cardImage.src = `Pic${data.url}`
+
+    return card;
+}

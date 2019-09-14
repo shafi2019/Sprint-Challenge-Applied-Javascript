1d3f5635a85c5de270e3eb47bbc4e97582ac1764
Shafi Masoumi <masoumi2017@icloud.com>
2019-09-14 10:47:19 -0700

    function created for Tab

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,36 +7,16 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
-let dataArray = [];
 
-const newData = axios.get('https://lambda-times-backend.herokuapp.com/topics')
-    .then (response => {
-        console.log(response.data);
-        dataArray = (response.data);
-    })
-    .catch(err => {
-        console.log(err);
-    })
-    
-    console.log(dataArray);
-
-    const newArray = Array.from(newData);
-    console.log(newArray);
-
-    const Topics = document.querySelector('.topics');
 
 function newTopics(data){
     const createTabs = document.createElement('div');
+
     createTabs.classList.add('tab');
+    
     createTabs.textContent = data;
+
     Topics.appendChild(createTabs);
 
     return Topics
 };
-
-axios.get('https://lambda-times-backend.herokuapp.com/topics')
-    .then((response) => {return response.data.topics})
-    .then((items) => {items.forEach((item) => newTopics(item))})
-    .catch(error => {
-        console.log(error);
-    });
